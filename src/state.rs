5bfbfcebd8b5d4f7dd7aa8c0a7ead850276fdf5e
Wulf0x67E7 <35270937+Wulf0x67E7@users.noreply.github.com>
2023-01-17 21:05:29 +0100

    trimmed down State::index

--- src/state.rs
+++ src/state.rs
@@ -2,11 +2,11 @@ use crate::pixel::Pixel;
 
 #[derive(Debug, Clone)]
 pub struct State {
-    pub(crate) index: [Pixel<4>; 256],
+    pub(crate) index: [Pixel<4>; 0x40],
     pub(crate) index_allowed: bool,
 }
 impl Default for State {
     fn default() -> Self {
-        Self { index: [Pixel::new(); 256], index_allowed: false }
+        Self { index: [Pixel::new(); 0x40], index_allowed: false }
     }
 }
